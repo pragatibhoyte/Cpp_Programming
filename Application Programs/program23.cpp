a359d25406bd9724c0985392cb96eef358e68c12
pragatibhoyte <pragatibhoyte73@gmail.com>
2026-06-25 21:05:03 +0530

    programs to accept and display details using Variations

---
+++ Application Programs/program23.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string *sName = NULL;
+    int iAge = 0;
+    float fMarks = 0.0f;
+
+    sName = new string;
+
+    cout<<"Enter your Name : \n";
+    getline(cin, *sName);
+
+    cout<<"Enter your Age : \n";
+    cin>>iAge;
+
+    cout<<"Enter your Marks : \n";
+    cin>>fMarks;
+
+    cout<<"Your Name is : "<<*sName<<endl;
+    cout<<"Your Age is : "<<iAge<<endl;
+    cout<<"Your Marks is : "<<fMarks<<endl;
+    
+    return 0;
+}
